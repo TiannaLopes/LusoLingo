29a5fc09888a0704327498c62e89a3dcdcacc1f5
TiannaLopes <tiannaalinalopes@gmail.com>
2024-02-02 14:34:00 +0200

    Initializig project

---
+++ LusoLingo/LusoLingo/LusoLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LusoLingoApp.swift
+//  LusoLingo
+//
+//  Created by Tianna Alina Lopes on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LusoLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
